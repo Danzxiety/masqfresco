e1bd6ad505560a3171a59add19e196b0fc0119ae
Daniel Aguilera <162569290+Danzxiety@users.noreply.github.com>
2024-03-23 16:13:48 -0400

    Create secrets.php

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51OoVv9BBw8oYUdLYx4DVA59KwMCPHBH3MFg1u2LBXzpr2HUop3xSWspKiwt6evTAJhbikUPcivaK4RIer2ifYpZd00qRL08MJA';
